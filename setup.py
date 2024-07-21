f5af4b82b0fd32171dbd6ec817b86ac2a68514d9
Pranav Zinzurde <pranavzinzurde@gmail.com>
2024-07-21 06:28:59 +0530

    type error

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ApnaHub',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'bcrypt',
+        'inquirer',
+        'mongoengine',
+        'tqdm',
+        'pyfiglet',
+        'termcolor'
+    ],
+    entry_points={
+        'console_scripts': [
+            'apnahub-server=server:server',
+            'apnahub-client=client:client',
+        ],
+    },
+    author='Apna Hub',
+    author_email='apnahub@gmail.com',
+    description='A CLI based P2P file sharing platform over LAN',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/navvvgupta/apna-hub',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
